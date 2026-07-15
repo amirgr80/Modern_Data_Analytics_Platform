4cb156291b49bbe5a5a34fccab2b769512acffa3
hoseinkashefi78 <hoseinkashefi78@gmail.com>
2026-07-15 18:00:08 +0330

    Tuple in transactional config

--- configs/spark/silver_transactional_config.py
+++ configs/spark/silver_transactional_config.py
@@ -1,4 +1,5 @@
 from typing import Final
+from typing import Final, Tuple
 
 
 BRONZE_BUCKET: Final[str] = "bronze"
@@ -7,7 +8,7 @@ BRONZE_TRANSACTIONAL_BASE_PATH: Final[str] = (
     "s3a://bronze/transactional"
 )
 
-SUPPORTED_TRANSACTIONAL_TABLES: Final[tuple[str, ...]] = (
+SUPPORTED_TRANSACTIONAL_TABLES: Final[Tuple[str, ...]] = (
     "categories",
     "order_items",
     "orders",
@@ -18,7 +19,7 @@ SUPPORTED_TRANSACTIONAL_TABLES: Final[tuple[str, ...]] = (
 
 BRONZE_PARTITION_COLUMN: Final[str] = "partition_date"
 
-BRONZE_TECHNICAL_COLUMNS: Final[tuple[str, ...]] = (
+BRONZE_TECHNICAL_COLUMNS: Final[Tuple[str, ...]] = (
     "_kafka_topic",
     "_kafka_partition",
     "_kafka_offset",
